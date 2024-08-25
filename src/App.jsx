48c712ba8a87cd7af74706ad4feb7de9dedb0e47
Rishu raj <21je0769@iitism.ac.in>
2024-08-25 15:36:08 +0530

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import reactLogo from './assets/react.svg'
+
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// import HomePage from './component/homePage'
+// import User from './component/user'
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route exact path="/" element={<HomePage />}>
+//         </Route>
+//         <Route path="user" element={<User />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./component/homePage";
+import User from "./component/user";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <h1> done</h1>
-    </>
-  )
+    <Router>
+      <Routes>
+        {/* Parent route */}
+        <Route path="/" element={<HomePage />}>
+          {/* Nested route */}
+        </Route>
+          <Route path="user" element={<User />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
