231653e71a648ee5f626b2e570cccdea5ff1d0c8
Rishu raj <21je0769@iitism.ac.in>
2024-08-28 19:03:05 +0530

    update navbar.jsx

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -4,23 +4,27 @@ const Navbar = ({ username, onCompare }) => {
   const [myUsername, setMyUsername] = useState("");
 
   const handleCompare = () => {
-    onCompare(myUsername);
+    if (myUsername.trim()) {
+      onCompare(myUsername);
+    }
   };
 
   return (
-    <nav className="bg-gray-800 p-4 flex justify-between items-center text-white">
-      <div className="text-lg font-semibold">{username}'s Solved Problems</div>
-      <div className="flex items-center">
+    <nav className="bg-gray-800 p-4 flex flex-col md:flex-row justify-between items-center text-white">
+      <div className="text-lg font-semibold hidden md:block">
+        {username}'s Solved Problems
+      </div>
+      <div className="flex flex-row items-center w-full md:w-auto">
         <input
           type="text"
           value={myUsername}
           onChange={(e) => setMyUsername(e.target.value)}
           placeholder="Enter your username"
-          className="mr-2 p-2 rounded bg-gray-200 text-gray-800"
+          className="p-2 rounded bg-gray-200 text-gray-800 w-3/5 md:w-auto mr-2"
         />
         <button
           onClick={handleCompare}
-          className="p-2 bg-customgreen text-white rounded"
+          className="p-2 bg-customgreen text-white rounded w-2/5 md:w-auto"
         >
           Compare
         </button>
