1d9d3abfea0af54d26ae7ef8fb6d9237a72aba53
Rishu raj <21je0769@iitism.ac.in>
2024-12-23 18:58:37 +0530

    updated problem for mobile version

--- src/component/homePage.jsx
+++ src/component/homePage.jsx
@@ -78,13 +78,15 @@ const HomePage = () => {
       <div className="absolute bottom-2 right-2 flex space-x-2">
         <div className="text-sm text-gray-600 justify-center font-bold">
           Owner :{" "}
-          <div
-            href="https://rishavrajj3225.github.io/Portfolio/"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm text-blue-600 hover:text-customGreen"
-          >
-            Rishu Raj{" "}
+          <div>
+            <a
+              href="https://rishavrajj3225.github.io/Portfolio/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-sm text-blue-600 hover:text-customGreen"
+            >
+              Rishu Raj{" "}
+            </a>
           </div>
           <span className="text-sm text-gray-600"></span>
           <a
@@ -93,7 +95,8 @@ const HomePage = () => {
             rel="noopener noreferrer"
             className="text-sm text-blue-600 hover:text-customGreen"
           >
-           {" "} Raj Shukla
+            {" "}
+            Raj Shukla
           </a>
         </div>
       </div>
